9a757e579dab3a6e2e0a04dfc86caabf0655aeda
Closer <1601312918@qq.com>
2024-06-21 19:28:18 +0800

    feat: add type module

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ bot.on('message', (msg) => {
 //   }
 // });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the preview environment!' })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
